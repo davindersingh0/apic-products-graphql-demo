84f530e506fe45dc0c922d5596e7582187ff49e4
dsing17 <davinder.singh@walmart.com>
2020-10-06 00:48:33 -0500

    first commit

---
+++ src/main/java/com/walmart/stratiaf/graphql/apicproductsgraphqldemo/ApicProductsGraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.stratiaf.graphql.apicproductsgraphqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicProductsGraphqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicProductsGraphqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/stratiaf/graphql/apicproductsgraphqldemo/ApicProductsGraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.stratiaf.graphql.apicproductsgraphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicProductsGraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
